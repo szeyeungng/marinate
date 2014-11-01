daa11f5addc11ef22d89bb443f2dcc8aff80c56b
macbook-jack <macbookair-jack@MacBookAir-Jacks-MacBook-Air.local>
2014-11-01 11:41:20 -0700

    my mongodb commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 // New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/textblogger');
+var db = monk('textblogger.herokuapp.com:27017/textblogger');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
