5e7d0eca15e987faaa6069f0fd074e7742d510ba
macbook-jack <macbookair-jack@MacBookAir-Jacks-MacBook-Air.local>
2014-11-01 02:28:42 -0700

    commitment3

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+//app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
