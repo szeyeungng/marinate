0c9f54fe23bf8398d7440782f7098876dcbdb82b
macbook-jack <macbookair-jack@MacBookAir-Jacks-MacBook-Air.local>
2014-11-01 02:06:57 -0700

    my halloween3 commit

--- routes/index.js
+++ routes/index.js
@@ -10,21 +10,14 @@ router.get('/helloworld',function(req,res){
 	res.render('helloworld',{title: 'Hello, World!'})
 });
 
-router.post('/sendsms',function(req,res){
-	var textBody = req.body.Body;
-	twiml.message(textBody);
-
-	res.type('text/xml');
-    res.send(twiml.toString());
-})
-
 router.get('/sms',function(req,res){
 	var client = require('twilio')('ACe2cfa86a5ecd532993d2ef687178c134','806a24e78fdacab45ebfc72960f1f1a4');
+	var textBody = req.body.Body;
 
 	client.sendMessage({
 		to:'+16502835564',
 		from:'+16503005260',
-		body:'Hi!'
+		body:textBody;
 	});
 });
 
