e961bf4577bb008f394920cc6ba250e476546d38
macbook-jack <macbookair-jack@MacBookAir-Jacks-MacBook-Air.local>
2014-11-01 02:52:10 -0700

    my db commit

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,31 @@ router.get('/helloworld',function(req,res){
 router.post('/sms',function(req,res){
 	var client = require('twilio')('ACe2cfa86a5ecd532993d2ef687178c134','806a24e78fdacab45ebfc72960f1f1a4');
 	var textBody = req.body.Body;
+	
+	var db = req.db;
+    // Get our form values. These rely on the "name" attributes
+    //var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    // Set our collection
+    var collection = db.get('usercollection');
 
-	client.sendMessage({
+	collection.insert({
+        "username" : textBody,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+
+    client.sendMessage({
 		to:'+16502835564',
 		from:'+16503005260',
 		body:'Hi' + textBody
