c7ef4755429428eb936d1e33d1a818b1c6f8fb10
macbook-jack <macbookair-jack@MacBookAir-Jacks-MacBook-Air.local>
2014-11-04 17:35:33 -0800

    mms

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.post('/sms',function(req,res){
 	var textBody = req.body.Body;
 	var textDate = req.body.DateSent;
 	var textFrom = req.body.From;
+	var textImg = req.body.MediaUrl;
 
 	var db = req.db;
 
@@ -23,6 +24,7 @@ router.post('/sms',function(req,res){
     client.sendMessage({
 		to: textFrom,
 		from:'+16503005260',
+		mediaUrl: textImg,
 		body:'Blog post from ' + textFrom + ' sent on ' + textDate + ' with content: ' + textBody
 	});
 });
