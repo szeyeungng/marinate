39b77e915afefe0cba34bd2e78c003f0dff7393a
Jack Ng <Jack@Jacks-MacBook-Pro-3.local>
2015-02-07 16:03:37 -0800

    bootstrap redesign

--- config/passport.js
+++ config/passport.js
@@ -64,15 +64,20 @@ module.exports = function(passport) {
             // we are checking to see if the user trying to login already exists
             User.findOne({ 'email' :  email }, function(err, user) {
                 // if there are any errors, return the error
-                if (err)
+                if (err){
                     return done(err);
-                if (!(phoneCode===phoneCodeEntered)){
-                    return done(null,false,req.flash('signupMessage','That code you entered is invalid.'));
                 }
                 // check to see if theres already a user with that email
                 if (user) {
                     return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-                } else {
+                }
+                if (!phoneNumber){
+                    return done(null,false,req.flash('signupMessage','Please enter a Phone Number.'));
+                }
+                if (!(phoneCode===phoneCodeEntered)){
+                    return done(null,false,req.flash('signupMessage','That code you entered is invalid.'));
+                }
+                else {
 
                     // if there is no user with that email
                     // create the user
