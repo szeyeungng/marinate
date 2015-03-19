af9fcd2ddc6e55e709d299e7601ef444aa5aa642
macbook-jack <jik.jack@gmail.com>
2015-03-18 18:03:47 -0700

    default page is login page

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.render('login.ejs', { message: req.flash('loginMessage') });
     });
 
     // =====================================
