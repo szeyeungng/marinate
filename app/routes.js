90d06bdcbd5b3607e8525d94f8a78bf8014bccce
macbook-jack <jik.jack@gmail.com>
2015-03-22 23:51:46 -0700

    uploader code added

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,14 @@
 var Entry = require('../app/models/entry');
 var Capsule = require('../app/models/capsule');
+
 var fs = require('fs');
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 
 // app/routes.js
-module.exports = function(app, passport) {
+module.exports = function(app, passport,aws) {
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
@@ -267,6 +271,38 @@ module.exports = function(app, passport) {
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
+    });
+
+    // uploader
+    app.get('/sign_s3', function(req, res){
+        aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+        var s3 = new aws.S3();
+        var s3_params = {
+            Bucket: S3_BUCKET,
+            Key: req.query.s3_object_name,
+            Expires: 60,
+            ContentType: req.query.s3_object_type,
+            ACL: 'public-read'
+        };
+        s3.getSignedUrl('putObject', s3_params, function(err, data){
+            if(err){
+                console.log(err);
+            }
+            else{
+                var return_data = {
+                    signed_request: data,
+                    url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
+                };
+                res.write(JSON.stringify(return_data));
+                res.end();
+            }
+        });
+    });
+
+    app.post('/submit_form', function(req, res){
+        avatar_url = req.body.avatar_url;
+        console.log(avatar_url); // TODO: create this function
+        // TODO: Return something useful or redirect
     });
 };
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // setup =================================================
 // tools
 var express = require('express');
+var http = require('http');
+var aws = require('aws-sdk');
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -12,8 +14,14 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var path = require('path');
 
 var multer = require('multer');
+
+//S3 Configuration
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET
 
 // configuration =========================================
 mongoose.connect('mongodb://szeyeungng:Password123!@ds029197.mongolab.com:29197/marinate');
@@ -26,6 +34,7 @@ app.use(cookieParser()); //read cookies
 app.use(bodyParser()); //get information from html forms
 
 app.engine('.html',require('ejs').__express);
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', __dirname + '/views');
 app.set('view engine','html');
 
@@ -38,7 +47,7 @@ app.use(flash()); //use connect-flash for flash messages stored in session
 app.use(multer({dest:'./uploads/'}));
 
 // routes =================================================
-require('./app/routes.js')(app,passport); //load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app,passport,aws); //load our routes and pass in our app and fully configured passport
 
 app.listen(port);
 /*var io = require('socket.io').listen(app.listen(port));*/
